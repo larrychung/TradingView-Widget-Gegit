18cc5bece8027b01dc3bbad4cbf10fb88154c16c
larrychung <82369845+larrychung@users.noreply.github.com>
2024-04-08 17:01:45 +0800

    Update TradingViewWidgetCSS-6Stockview-2024-02-26.user.js

--- TradingViewWidgetCSS-6Stockview-2024-02-26.user.js
+++ TradingViewWidgetCSS-6Stockview-2024-02-26.user.js
@@ -6,9 +6,29 @@
 // @author       You
 // @match        https://*.tradingview.com/chart/*
 // @icon         https://static.tradingview.com/static/images/favicon.ico
+
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_addStyle
+// @grant        GM_setClipboard
+/// @grant        GM_getClipboard
+// @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
+// @grant        GM_openInTab
+// @grant                GM_deleteValue
+// @grant                GM_registerMenuCommand
+// @grant                GM_notification
+// @grant                GM_download
+// @grant                GM.getValue
+// @grant                GM.setValue
+// @grant                GM.deleteValue
+// @grant                GM.addStyle
+// @grant                GM.openInTab
+// @grant                GM.setClipboard
+// @grant                GM.xmlHttpRequest
+// @grant                GM.registerMenuCommand
+// @grant                GM.notification
+
 // @run-at       document-end
 // @sandbox      true
 
@@ -16,34 +36,61 @@
 
 
 //------------------------------------------------------------------------TradingView彈出小圖-------------------------------------------------------------------------------
-        // 假设 idTV 是你要引用的股票代码
+// 假设 idTV 是你要引用的股票代码
 (function() {
     'use strict';
 
-        const idTV = 'NASDAQ:GOOG';
+    const idTV = "NYSE:V";
 
-        // 创建一个函数来添加CSS2样式
-        let stylesAdded = false;
 
-        // 创建一个函数来添加CSS样式
-        function addCSS(cssContent, identifier) {
-            if (!stylesAdded) {
-                var styleElement = document.createElement('style');
-                styleElement.textContent = cssContent;
-                styleElement.setAttribute('data-identifier', identifier);
-                document.head.appendChild(styleElement);
-                stylesAdded = true;
-            }
+
+// 读取剪贴板内容并保存到GM存储中
+function readClipboardAndSave() {
+    navigator.clipboard.readText()
+        .then(text => {
+            // 将剪贴板内容保存到GM存储中
+            GM_setValue('clipboardContent', text);
+            console.log('剪贴板内容已保存:', text);
+        })
+        .catch(error => {
+            console.error('无法读取剪贴板内容:', error);
+        });
+}
+
+// 监听剪贴板内容变化
+document.addEventListener('clipboard', readClipboardAndSave);
+
+// 初次加载时也调用一次以保存当前剪贴板内容
+readClipboardAndSave();
+
+
+
+
+
+
+
+    // 创建一个函数来添加CSS2样式
+    let stylesAdded = false;
+
+    // 创建一个函数来添加CSS样式
+    function addCSS(cssContent, identifier) {
+        if (!stylesAdded) {
+            var styleElement = document.createElement('style');
+            styleElement.textContent = cssContent;
+            styleElement.setAttribute('data-identifier', identifier);
+            document.head.appendChild(styleElement);
+            stylesAdded = true;
         }
+    }
 
-        // Add custom style using GM_addstylecss 強制使用格式
-        let customBackgroundColor61 = "#120903";
-        let customBackgroundColor62 = "#5e2802";
-        GM_addStyle(".tradingview-popup-script61{background-color: " + customBackgroundColor61 + " !important }");
-        GM_addStyle(".tradingview-popup-script62{background-color: " + customBackgroundColor62 + " !important }");
+    // Add custom style using GM_addstylecss 強制使用格式
+    let customBackgroundColor61 = "#120903";
+    let customBackgroundColor62 = "#5e2802";
+    GM_addStyle(".tradingview-popup-script61{background-color: " + customBackgroundColor61 + " !important }");
+    GM_addStyle(".tradingview-popup-script62{background-color: " + customBackgroundColor62 + " !important }");
 
-        // Add CSS stylecsss for the popup
-        addCSS(`
+    // Add CSS stylecsss for the popup
+    addCSS(`
         .tradingview-popup-script61{
             position: absolute;
             top: 50%;
@@ -59,11 +106,11 @@
         }
     `, 'tradingview-popup-scrip61');
 
-        // Create a function to add the TradingView widget
-        function addTradingViewWidget() {
-            var container = document.createElement('div');
-            container.className = 'tradingview-popup-script61';
-            container.innerHTML = `
+    // Create a function to add the TradingView widget
+    function addTradingViewWidget() {
+        var container = document.createElement('div');
+        container.className = 'tradingview-popup-script61';
+        container.innerHTML = `
             <div class="tradingview-widget-container61" >
                 <div class="tradingview-widget-container__widget" ></div>
                 <div class="tradingview-widget-copyright">
@@ -74,14 +121,14 @@
             </div>
         `;
 
-            document.body.appendChild(container);
+        document.body.appendChild(container);
 
-            // Load the TradingView widget script
-            var script = document.createElement('script');
-            script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js';
-            script.async = true;
-            // 将 "${idTV}" 插入到 symbol 字段中
-            script.innerHTML = `
+        // Load the TradingView widget script
+        var script = document.createElement('script');
+        script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js';
+        script.async = true;
+        // 将 "${idTV}" 插入到 symbol 字段中
+        script.innerHTML = `
            {
             "autosize": true,
             "symbol": "${idTV}",
@@ -99,106 +146,106 @@
             "support_host": "https://www.tradingview.com"
           }
         `;
-            container.appendChild(script);
+        container.appendChild(script);
 
-            // Add event listeners for clicking
-            container.addEventListener('click', function() {
-                container.style.zIndex = getMaxZIndex() + 1;
-            });
+        // Add event listeners for clicking
+        container.addEventListener('click', function() {
+            container.style.zIndex = getMaxZIndex() + 1;
+        });
 
-            // 獲取文檔中最高的z-index值
-            function getMaxZIndex() {
-                var elements = document.getElementsByTagName('*');
-                var maxIndex = 0;
-                for (var i = 0; i < elements.length; i++) {
-                    var zIndex = parseInt(window.getComputedStyle(elements[i]).zIndex);
-                    if (!isNaN(zIndex) && zIndex > maxIndex) {
-                        maxIndex = zIndex;
-                    }
-                }
-                return maxIndex;
-            }
-
-            // Add event listeners for dragging
-            var offsetX, offsetY, isDragging = false;
-            container.addEventListener('mousedown', function(e) {
-                isDragging = true;
-                offsetX = e.clientX - container.offsetLeft;
-                offsetY = e.clientY - container.offsetTop;
-            });
-            document.addEventListener('mousemove', function(e) {
-                if (isDragging) {
-                    container.style.left = e.clientX - offsetX + 'px';
-                    container.style.top = e.clientY - offsetY + 'px';
-                }
-            });
-            document.addEventListener('mouseup', function() {
-                isDragging = false;
-            });
-
-            // Create a function to toggle the CSS window size
-            function toggleCSSWindowSize() {
-                if (container.style.width === '350px') {
-                    container.style.width = '980px';
-                    container.style.height = '610px';
-                } else {
-                    container.style.width = '600px';
-                    container.style.height = '400px';
+        // 獲取文檔中最高的z-index值
+        function getMaxZIndex() {
+            var elements = document.getElementsByTagName('*');
+            var maxIndex = 0;
+            for (var i = 0; i < elements.length; i++) {
+                var zIndex = parseInt(window.getComputedStyle(elements[i]).zIndex);
+                if (!isNaN(zIndex) && zIndex > maxIndex) {
+                    maxIndex = zIndex;
                 }
             }
-
-            // Create a button to toggle the CSS window size
-            var toggleButton = document.createElement('button');
-            toggleButton.textContent = 'T';
-            toggleButton.style.position = 'absolute';
-            toggleButton.style.top = '10px';
-            toggleButton.style.right = '10px';
-            toggleButton.style.zIndex = '10000'; // 修正了z-index值
-            toggleButton.addEventListener('click', toggleCSSWindowSize);
-            // Add the button to the container
-            container.appendChild(toggleButton);
-
-            // Create a function to hide the CSS window
-            function hideCSSWindow() {
-                container.style.display = 'none';
-                document.body.appendChild(restoreButton);
-            }
-
-            // Create a button to hide the CSS window
-            var hideButton = document.createElement('button');
-            hideButton.textContent = 'H';
-            hideButton.style.position = 'absolute';
-            hideButton.style.top = '10px';
-            hideButton.style.right = '40px';
-            hideButton.style.zIndex = '10000'; // 修正了z-index值
-            hideButton.addEventListener('click', hideCSSWindow);
-            hideButton.style.backgroundColor = customBackgroundColor62;
-            // Add the button to the container
-            container.appendChild(hideButton);
-
-            // Create a function to restore the CSS window
-            function restoreCSSWindow() {
-                container.style.display = 'block';
-                document.body.removeChild(restoreButton);
-            }
-
-            // Create a button to restore the CSS window
-            var restoreButton = document.createElement('button');
-            restoreButton.textContent = 'LIT';
-            restoreButton.style.position = 'absolute';
-            restoreButton.style.bottom = '10px';
-            restoreButton.style.left = '282px';
-            restoreButton.style.zIndex = '10000'; // 修正了z-index值
-            restoreButton.addEventListener('click', restoreCSSWindow);
-            restoreButton.style.borderRadius = '8px';
-            restoreButton.style.backgroundColor = customBackgroundColor62;
-            // Add the button to the container
-            container.appendChild(restoreButton);
-
-            // Hide the CSS window when it's created
-            hideCSSWindow();
+            return maxIndex;
         }
 
-        // Call the function to add the TradingView widget
-        addTradingViewWidget();
+        // Add event listeners for dragging
+        var offsetX, offsetY, isDragging = false;
+        container.addEventListener('mousedown', function(e) {
+            isDragging = true;
+            offsetX = e.clientX - container.offsetLeft;
+            offsetY = e.clientY - container.offsetTop;
+        });
+        document.addEventListener('mousemove', function(e) {
+            if (isDragging) {
+                container.style.left = e.clientX - offsetX + 'px';
+                container.style.top = e.clientY - offsetY + 'px';
+            }
+        });
+        document.addEventListener('mouseup', function() {
+            isDragging = false;
+        });
+
+        // Create a function to toggle the CSS window size
+        function toggleCSSWindowSize() {
+            if (container.style.width === '350px') {
+                container.style.width = '980px';
+                container.style.height = '610px';
+            } else {
+                container.style.width = '600px';
+                container.style.height = '400px';
+            }
+        }
+
+        // Create a button to toggle the CSS window size
+        var toggleButton = document.createElement('button');
+        toggleButton.textContent = 'T';
+        toggleButton.style.position = 'absolute';
+        toggleButton.style.top = '10px';
+        toggleButton.style.right = '10px';
+        toggleButton.style.zIndex = '10000'; // 修正了z-index值
+        toggleButton.addEventListener('click', toggleCSSWindowSize);
+        // Add the button to the container
+        container.appendChild(toggleButton);
+
+        // Create a function to hide the CSS window
+        function hideCSSWindow() {
+            container.style.display = 'none';
+            document.body.appendChild(restoreButton);
+        }
+
+        // Create a button to hide the CSS window
+        var hideButton = document.createElement('button');
+        hideButton.textContent = 'H';
+        hideButton.style.position = 'absolute';
+        hideButton.style.top = '10px';
+        hideButton.style.right = '40px';
+        hideButton.style.zIndex = '10000'; // 修正了z-index值
+        hideButton.addEventListener('click', hideCSSWindow);
+        hideButton.style.backgroundColor = customBackgroundColor62;
+        // Add the button to the container
+        container.appendChild(hideButton);
+
+        // Create a function to restore the CSS window
+        function restoreCSSWindow() {
+            container.style.display = 'block';
+            document.body.removeChild(restoreButton);
+        }
+
+        // Create a button to restore the CSS window
+        var restoreButton = document.createElement('button');
+        restoreButton.textContent = 'LIT';
+        restoreButton.style.position = 'absolute';
+        restoreButton.style.bottom = '10px';
+        restoreButton.style.left = '282px';
+        restoreButton.style.zIndex = '10000'; // 修正了z-index值
+        restoreButton.addEventListener('click', restoreCSSWindow);
+        restoreButton.style.borderRadius = '8px';
+        restoreButton.style.backgroundColor = customBackgroundColor62;
+        // Add the button to the container
+        container.appendChild(restoreButton);
+
+        // Hide the CSS window when it's created
+        hideCSSWindow();
+    }
+
+    // Call the function to add the TradingView widget
+    addTradingViewWidget();
 })();
